43df401066276124062b69c4dd654de5a8c94069
Sam Pai <sampai@sams-mbp.lan>
2019-07-16 22:08:14 -0700

    completed game, still needs styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,6 @@ function randomIntFromInterval(min,max) // min and max included
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 var numberGoal = randomIntFromInterval(19,120);
-console.log(numberGoal);
 var crystalValues1 = randomIntFromInterval(1,12);
 var crystalValues2 = randomIntFromInterval(1,12);
 var crystalValues3 = randomIntFromInterval(1,12);
@@ -18,16 +17,73 @@ var counter = 0;
 var wins = 1;
 var losses = 1;
 
-
+game();
 
 function game() {
-// this is where the game functionality . past on click functions
+    $("#crystalOne").on("click", function(){
+
+        thisValue = crystalValues1
+        $("#score2").text(counter += thisValue);
+        
+     })
+     
+     
+     $("#crystalTwo").on("click", function(){
+     
+         thisValue = crystalValues2
+         $("#score2").text(counter += thisValue);
+      
+      })
+      $("#crystalThree").on("click", function(){
+     
+         thisValue = crystalValues3
+         $("#score2").text(counter += thisValue);
+      
+      })
+      $("#crystalFour").on("click", function(){
+     
+         thisValue = crystalValues4
+         $("#score2").text(counter += thisValue);
+      
+      })
+     
+      $("#crystalOne, #crystalTwo, #crystalThree, #crystalFour").on("click", function(){
+         
+        if (counter === numberGoal){
+            $("#resultText").text("You Won!");
+            $("#winsCounter").text("Wins: " + wins++);
+            //  swal("YOU WON!");
+             setTimeout(reset(), 1000);
+             
+      }
+         else if (counter > numberGoal){
+             $("#resultText").text("You Lost!!");
+
+             $("#lossCounter").text("Losses: " + losses++);
+
+            //  swal("Sorry, You lost!");
+            setTimeout(reset(), 1000);
+
+         }
+         }
+     
+     )
 }
+
+
+
+
+$("#numberToHit").text(numberGoal);
 
 function reset(){
     counter = 0;
     numberGoal = randomIntFromInterval(19,120);
-    $("#resultText").html("");
+    crystalValues1 = randomIntFromInterval(1,12);
+    crystalValues2 = randomIntFromInterval(1,12);
+    crystalValues3 = randomIntFromInterval(1,12);
+    crystalValues4 = randomIntFromInterval(1,12);
+    // $("#resultText").html("");
+    setTimeout(test, 2000);
     $("#numberToHit").text(numberGoal);
     $("#score2").text(counter);
 
@@ -35,47 +91,7 @@ function reset(){
 
 }
 
-$("#crystalOne").on("click", function(){
-
-   thisValue = crystalValues1
-   $("#score2").text(counter += thisValue);
-   
-})
-
-
-$("#crystalTwo").on("click", function(){
-
-    thisValue = crystalValues2
-    $("#score2").text(counter += thisValue);
- 
- })
- $("#crystalThree").on("click", function(){
-
-    thisValue = crystalValues3
-    $("#score2").text(counter += thisValue);
- 
- })
- $("#crystalFour").on("click", function(){
-
-    thisValue = crystalValues4
-    $("#score2").text(counter += thisValue);
- 
- })
-
- $("#crystalOne, #crystalTwo, #crystalThree, #crystalFour").on("click", function(){
-    if (counter === numberGoal){
-        $("#winsCounter").text("Wins: " + wins++);
-        $("#resultText").text("You Won!");
-        
- }
-    else if (counter > numberGoal){
-        $("#lossCounter").text("Losses: " + losses++);
-        $("#resultText").text("You Lost!!");
-        reset();
-    }
-    }
-
-)
-$("#numberToHit").text(numberGoal);
-
+function test (){
+    $("#resultText").html("")
+}
 })
